7f592379465307b4255dc262620e8d76ffcc1662
Rora Alem <rora.goitom@gmail.com>
2023-10-24 13:59:32 -0400

    Add gets.chomp to get input  and puts for output

--- main.rb
+++ main.rb
@@ -2,3 +2,8 @@ require_relative 'player'
 require_relative 'game'
 require_relative 'question'
 
+puts "Welcome to the Math Game!"
+puts "Enter Player 1's name: "
+player1 = Player.new(gets.chomp)
+puts "Enter Player 2's name: "
+player2 = Player.new(gets.chomp)
